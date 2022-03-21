20391ec070b17b912cb2dc5d862d980abc565518
gaon12 <gokirito12@gmail.com>
2022-03-21 17:52:33 +0900

    압축/압축 해제 추가 #4

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib, "Urlmon.lib")
 #include <chrono>
 #include <filesystem>
+#include <winbase.h>
 using namespace std;
 
 int main() {
@@ -37,7 +38,7 @@ TERMS:
 
     char terms_agree;
 
-    scanf_s("%c", &terms_agree, sizeof(1));
+    scanf_s("%c", &terms_agree);
     if (terms_agree == 'Y' || terms_agree == 'y') {
         goto START;
     }
@@ -53,12 +54,12 @@ START:
 
     std::cout << "벤치마크 프로그램입니다.\n필요한 파일들을 다운로드하고 있습니다. 잠시만 기다려주세요.\n" << std::endl;
     std::cout << "방화벽 등의 프로그램에서 차단하고 있다면 예외 처리해 주세요!\n\n" << std::endl;
-
     URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/7-Zip/7z.exe", L"7z.exe", 0, NULL); //7z.exe를 다운로드합니다.
     URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/7-Zip/7z.dll", L"7z.dll", 0, NULL); //7z.exe이 동작하는데 필요한 파일인 7z.dll 파일을 다운로드합니다.
     URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/waifu2x/waifu2x-converter-cpp/wcc.zip", L"wcc.zip", 0, NULL); //waifu2x cpp 버전을 다운로드합니다.
     system("@echo off && 7z x wcc.zip > 1.txt"); //다운로드 받은 파일을 압축해제합니다.
     URLDownloadToFile(NULL, L"https://i.ibb.co/1b2Ns1b/aki.png", L"aki.png", 0, NULL);//크기가 작은 이미지 파일을 다운로드합니다.
+    URLDownloadToFile(NULL, L"https://common.gaon.xyz/prj/cpp_cli_benchmark/zip.7z", L"zip.7z", 0, NULL); //테스트용 압축 파일을 다운로드합니다.
     Sleep(600);
     std::cout << "현재 수행중인 작업은 다음과 같습니다.\n\n" << std::endl;
     Sleep(800);
@@ -88,7 +89,7 @@ START:
 
     double pi = 0.0, num = 1.0;
 
-    while (num < 100000000)
+    while (num < 1000000000)
     {
         pi += 1.0 / pow(num, 2.0);
         num += 1.0;
@@ -123,6 +124,40 @@ START:
     chrono::seconds sec_fourth = chrono::duration_cast<chrono::seconds>(FourthPartEndTime - ThirdPartEndTime);
     cout << mill_fourth.count() << "ms" << "(" << sec_fourth.count() << "초)" << endl;//시작 출력
 
+    system("title 벤치마크 - (작업) 압축/압축 해제 작업"); //콘솔창 제목 설정
+
+    std::cout << "\n5. 압축/압축 해제 작업 : " << std::endl;
+    
+    /*
+    압축과 압축 해제 작업입니다. 아래의 순서대로 작업이 이루어집니다.
+
+    1. 먼저 실행되고 있는 PC의 CPU 스레드 수를 획득합니다.
+    2. 그리고 7z를 이용하여 다운로드 받은 zip.7z를 압축해제 합니다.
+    3. 2의 작업으로 얻은 zip 폴더를 test.7z라는 파일로 압축합니다. 이때 압축시 아까 얻은 CPU 스레드 수만큼 압축 작업을 합니다.
+       (즉, 모든 스레드가 압축하는데 사용하도록 합니다.)
+    4. 마찬가지로 2의 작업으로 얻은 zip 폴더를 test.zip이라는 파일로 압축합니다. 마찬가지로 모든 스레드를 압축 작업에 사용합니다.
+    5. zip 폴더를 삭제합니다. test.zip 파일을 압축해제하면 zip폴더가 생성이 되기 때문에 압축해제 하기 전에 zip 폴더를 삭제합니다.
+    6. 4의 작업으로 얻은 test.zip을 압축해제합니다.
+    */
+    SYSTEM_INFO siSysInfo; // Copy the hardware information to the SYSTEM_INFO structure. 
+    GetSystemInfo(&siSysInfo); // Display the contents of the SYSTEM_INFO structure. 
+
+    char cmd1[100];
+    char cmd2[100];
+    system("7z x -bd zip.7z > 1.txt"); //zip.7z 파일을 압축해제합니다.
+    sprintf_s(cmd1, "7z a -t7z -mmt%u -mx9 test.7z \"zip\" > 1.txt", siSysInfo.dwNumberOfProcessors); //zip 폴더를 7z로 압축합니다.
+    system(cmd1);
+    sprintf_s(cmd2, "7z a -tzip -mmt%u -mx9 test.zip \"zip\" > 1.txt", siSysInfo.dwNumberOfProcessors); //zip 폴더를 zip으로 압축합니다.
+    system(cmd2);
+    system("del /s /q zip > 1.txt");
+    system("7z x -bd zip.7z > 1.txt");
+
+    chrono::system_clock::time_point FifthPartEndTime = chrono::system_clock::now();
+    chrono::duration<double> DefaultSec_Fifth = FifthPartEndTime - FourthPartEndTime;
+    chrono::milliseconds mill_fifth = chrono::duration_cast<chrono::milliseconds>(FifthPartEndTime - FourthPartEndTime);
+    chrono::seconds sec_fifth = chrono::duration_cast<chrono::seconds>(FifthPartEndTime - FourthPartEndTime);
+    cout << mill_fifth.count() << "ms" << "(" << sec_fifth.count() << "초)" << endl;//시작 출력
+
 
     chrono::system_clock::time_point EndTime = chrono::system_clock::now();
     chrono::duration<double> DefaultSec_end = EndTime - StartTime;
@@ -135,7 +170,7 @@ START:
 
     //사용한 파일들 삭제
     system("@echo off && del terms.txt 7z.exe 7z.dll wcc.zip waifu2x-converter-cpp.exe w2xc.dll opencv_world430.dll LICENSE aki.png aki_out.png testfile.bin > 1.txt");
-    system("@echo off && del /s /q models_rgb > 1.txt"); //폴더는 따로 삭제합니다.
+    system("@echo off && del /s /q models_rgb && del /s /q zip > 1.txt"); //폴더는 따로 삭제합니다.
 
     char filename1[] = "1.txt";
 
