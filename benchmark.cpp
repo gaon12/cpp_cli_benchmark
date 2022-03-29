caf89678c811e1f8b51a5b86fa6983cffefcaad9
gaon12 <gokirito12@gmail.com>
2022-03-29 22:47:02 +0900

    수치 조정, 고용량 파일 읽기/변환 #10 , 시간 쉽게 읽을 수 있도록 변경, 다운로드 한번에 받아서 압축해제 하도록 개선 #12 , 잔류폴더 삭제

--- benchmark.cpp
+++ benchmark.cpp
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <winbase.h>
 #include "unistd.h"
+#include <direct.h>
 using namespace std;
 
 int main() {
@@ -49,29 +50,29 @@ TERMS:
 START:
     
     system("cls");
-
+    
     std::cout << "벤치마크 프로그램입니다.\n필요한 파일들을 다운로드하고 있습니다. 잠시만 기다려주세요.\n" << std::endl;
     std::cout << "방화벽 등의 프로그램에서 차단하고 있다면 예외 처리해 주세요!\n\n" << std::endl;
     URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/7-Zip/7z.exe", L"7z.exe", 0, NULL); //7z.exe를 다운로드합니다.
     URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/7-Zip/7z.dll", L"7z.dll", 0, NULL); //7z.exe이 동작하는데 필요한 파일인 7z.dll 파일을 다운로드합니다.
-    URLDownloadToFile(NULL, L"https://common.gaon.xyz/utils/waifu2x/waifu2x-converter-cpp/wcc.zip", L"wcc.zip", 0, NULL); //waifu2x cpp 버전을 다운로드합니다.
-    system("@echo off && 7z x wcc.zip > 1.txt"); //다운로드 받은 파일을 압축해제합니다.
+    URLDownloadToFile(NULL, L"https://common.gaon.xyz/prj/cpp_cli_benchmark/all.7z", L"all.7z", 0, NULL); //waifu2x, 테스트용 압축 파일이 담긴 압축파일을 다운로드합니다.
+    system("@echo off && 7z x all.7z > 1.txt"); //다운로드 받은 파일을 압축해제합니다.
     URLDownloadToFile(NULL, L"https://i.ibb.co/1b2Ns1b/aki.png", L"aki.png", 0, NULL);//크기가 작은 이미지 파일을 다운로드합니다.
-    URLDownloadToFile(NULL, L"https://common.gaon.xyz/prj/cpp_cli_benchmark/zip.7z", L"zip.7z", 0, NULL); //테스트용 압축 파일을 다운로드합니다.
+    
     Sleep(600);
     std::cout << "현재 수행중인 작업은 다음과 같습니다.\n\n" << std::endl;
     Sleep(800);
-
+    
     system("title 벤치마크 - (연산) 1부터 600억까지 더하기"); //콘솔창 제목 설정
 
-    std::cout << "1. 1부터 600억까지 더하기" << std::endl;
+    std::cout << "1. 1부터 300억까지 더하기" << std::endl;
     
     long sum = 0;
     chrono::system_clock::time_point StartTime = chrono::system_clock::now();//시간 계산 시작
     
     long long i = 0;
     long long sums = 0;
-    for (i = 1; i <= 60000000000; i++) { //1부터 60000000000까지 반복
+    for (i = 1; i <= 30000000000; i++) { //1부터 30000000000까지 반복
         sums += i; //sums=sums+i
     }
     
@@ -87,7 +88,7 @@ START:
 
     double pi = 0.0, num = 1.0;
 
-    while (num < 2000000000)
+    while (num < 1000000000)
     {
         pi += 1.0 / pow(num, 2.0);
         num += 1.0;
@@ -111,10 +112,10 @@ START:
     chrono::seconds sec_third = chrono::duration_cast<chrono::seconds>(ThirdPartEndTime - SecondPartEndTime);
     cout << mill_third.count() << "ms" << "(" << sec_third.count() << "초)" << endl;//시간 출력
 
-    system("title 벤치마크 - (작업) waifu2x 40배 업스케일링 작업"); //콘솔창 제목 설정
+    system("title 벤치마크 - (작업) waifu2x 30배 업스케일링 작업"); //콘솔창 제목 설정
 
-    std::cout << "\n4. 머신러닝(SRCNN) 기반 이미지 업스케일링 작업(40배) : " << std::endl;
-    system("@echo off && waifu2x-converter-cpp  --scale-ratio 40 -i aki.png -o aki_out.png > 1.txt");
+    std::cout << "\n4. 머신러닝(SRCNN) 기반 이미지 업스케일링 작업(30배) : " << std::endl;
+    system("@echo off && waifu2x-converter-cpp  --scale-ratio 30 -i aki.png -o aki_out.png > 1.txt");
 
     chrono::system_clock::time_point FourthPartEndTime = chrono::system_clock::now();
     chrono::duration<double> DefaultSec_Fourth = FourthPartEndTime - ThirdPartEndTime;
@@ -156,6 +157,31 @@ START:
     chrono::seconds sec_fifth = chrono::duration_cast<chrono::seconds>(FifthPartEndTime - FourthPartEndTime);
     cout << mill_fifth.count() << "ms" << "(" << sec_fifth.count() << "초)" << endl;//시작 출력
 
+    system("title 벤치마크 - (작업) 고용량 파일 읽기/변환"); //콘솔창 제목 설정
+
+    std::cout << "\n6. 고용량 파일 읽기/변환 : " << std::endl;
+
+    system("fsutil file createnew 100MB.txt 104857600 > 1.txt"); //100MB짜리 더미파일을 생성합니다.
+
+    FILE* fp2 = 0;
+    char text2[100];
+    setlocale(LC_ALL, "korean");
+    fopen_s(&fp2, "100MB.txt", "r");
+    while (1) {
+        fgets(text2, 100, fp2);
+        if (feof(fp2)) break;
+    }
+    fclose(fp2); //파일 닫기
+
+    system("certutil -encode aki_out.jpg aki.b64 > 1.txt"); //base64 인코딩합니다.
+    system("certutil -decode aki.b64 aki_out_b64.jpg > 1.txt"); //base64 디코딩합니다.
+
+    chrono::system_clock::time_point SixthPartEndTime = chrono::system_clock::now();
+    chrono::duration<double> DefaultSec_Sixth = SixthPartEndTime - FifthPartEndTime;
+    chrono::milliseconds mill_sixth = chrono::duration_cast<chrono::milliseconds>(SixthPartEndTime - FifthPartEndTime);
+    chrono::seconds sec_sixth = chrono::duration_cast<chrono::seconds>(SixthPartEndTime - FifthPartEndTime);
+    cout << mill_sixth.count() << "ms" << "(" << sec_sixth.count() << "초)" << endl;//시작 출력
+
     chrono::system_clock::time_point EndTime = chrono::system_clock::now();
     chrono::duration<double> DefaultSec_end = EndTime - StartTime;
     chrono::milliseconds mill_end = chrono::duration_cast<chrono::milliseconds>(EndTime - StartTime);
@@ -163,16 +189,27 @@ START:
     chrono::minutes min_end = chrono::duration_cast<chrono::minutes>(EndTime - StartTime);
 
     std::cout << "\n총 수행시간 : " << std::endl;
+
+    int min, hour;
+    min = sec_end.count() / 60;
+    hour = min / 60;
+    min = min % 60;
+
     cout << mill_end.count() << "ms (" << sec_end.count() << "초)" << endl;//시간 출력
+    cout << "총 걸린 시간은 " << hour << "시간 " << min << "분 " << sec_end.count() % 60 << "초 입니다." << endl;
 
     //사용한 파일들 삭제
-    system("@echo off && del terms.txt 7z.exe 7z.dll wcc.zip waifu2x-converter-cpp.exe w2xc.dll opencv_world430.dll LICENSE aki.png aki_out.png testfile.bin test.zip test.7z zip.7z > 1.txt");
+    system("@echo off && del terms.txt 7z.exe 7z.dll all.7z waifu2x-converter-cpp.exe w2xc.dll opencv_world430.dll LICENSE aki.png aki_out.png testfile.bin test.zip test.7z zip.7z 500MB.txt, aki.b64, aki_out_b64.jpg > 1.txt");
     system("@echo off && del /s /q models_rgb > 1.txt"); //폴더는 따로 삭제합니다.
     system("@echo off && del /s /q zip > 1.txt");
 
     char filename1[] = "1.txt";
 
     int r;
-
     r = _unlink(filename1);
+
+    char rmpath1[] = { "models_rgb" };
+    char rmpath2[] = { "zip" };
+    int remove1 = rmdir(rmpath1);
+    int remove2 = rmdir(rmpath2);
 }
